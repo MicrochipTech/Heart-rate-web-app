074ef943f405b3c3d72dd90af1ab78ae9378c709
Abdelrahman Diab - C43071 <abdelrahman.diab@microchip.com>
2019-05-19 13:04:44 -0700

    debug

--- public/javascripts/beats.js
+++ public/javascripts/beats.js
@@ -1,31 +1,19 @@
 $(document).ready(function () {
   var timeData = [],
-    temperatureData = [],
-    heartrateData = [];
+    heartbeatData = [];
   var data = {
     labels: timeData,
     datasets: [
       {
         fill: false,
-        label: 'Temperature',
-        yAxisID: 'Temperature',
-        borderColor: "rgba(255, 204, 0, 1)",
-        pointBoarderColor: "rgba(255, 204, 0, 1)",
-        backgroundColor: "rgba(255, 204, 0, 0.4)",
-        pointHoverBackgroundColor: "rgba(255, 204, 0, 1)",
-        pointHoverBorderColor: "rgba(255, 204, 0, 1)",
-        data: temperatureData
-      },
-      {
-        fill: false,
-        label: 'Heart Rate',
-        yAxisID: 'Heart Rate',
+        label: 'Beats',
+        yAxisID: 'Beats',
         borderColor: "rgba(24, 120, 240, 1)",
         pointBoarderColor: "rgba(24, 120, 240, 1)",
         backgroundColor: "rgba(24, 120, 240, 0.4)",
         pointHoverBackgroundColor: "rgba(24, 120, 240, 1)",
         pointHoverBorderColor: "rgba(24, 120, 240, 1)",
-        data: heartrateData
+        data: heartbeatData
       }
     ]
   }
@@ -84,24 +72,18 @@ $(document).ready(function () {
     console.log('receive message' + message.data);
     try {
       var obj = JSON.parse(message.data);
-      if(!obj.time || !obj.temperature) {
-        return;
-      }
+      
       timeData.push(obj.time);
-      temperatureData.push(obj.temperature);
+
       // only keep no more than 50 points in the line chart
-      const maxLen = 50;
+      const maxLen = 20;
       var len = timeData.length;
-      if (len > maxLen) {
-        timeData.shift();
-        temperatureData.shift();
-      }
 
       if (obj.humidity) {
-        heartrateData.push(obj.humidity);
+        heartbeatData.push(obj.humidity);
       }
-      if (heartrateData.length > maxLen) {
-        heartrateData.shift();
+      if (heartbeatData.length > maxLen) {
+        heartbeatData.shift();
       }
 
       myLineChart.update();
