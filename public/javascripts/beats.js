74f4cf5a18045a48397f688507c5ecfa880312b7
Abdelrahman Diab - C43071 <abdelrahman.diab@microchip.com>
2019-05-19 13:06:03 -0700

    debug

--- public/javascripts/beats.js
+++ public/javascripts/beats.js
@@ -6,14 +6,25 @@ $(document).ready(function () {
     datasets: [
       {
         fill: false,
-        label: 'Beats',
-        yAxisID: 'Beats',
+        label: 'Temperature',
+        yAxisID: 'Temperature',
+        borderColor: "rgba(255, 204, 0, 1)",
+        pointBoarderColor: "rgba(255, 204, 0, 1)",
+        backgroundColor: "rgba(255, 204, 0, 0.4)",
+        pointHoverBackgroundColor: "rgba(255, 204, 0, 1)",
+        pointHoverBorderColor: "rgba(255, 204, 0, 1)",
+        data: temperatureData
+      },
+      {
+        fill: false,
+        label: 'Heart Rate',
+        yAxisID: 'Heart Rate',
         borderColor: "rgba(24, 120, 240, 1)",
         pointBoarderColor: "rgba(24, 120, 240, 1)",
         backgroundColor: "rgba(24, 120, 240, 0.4)",
         pointHoverBackgroundColor: "rgba(24, 120, 240, 1)",
         pointHoverBorderColor: "rgba(24, 120, 240, 1)",
-        data: heartbeatData
+        data: heartrateData
       }
     ]
   }
