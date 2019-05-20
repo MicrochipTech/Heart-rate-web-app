4d81750da5e98bee5a82ca26674a2e216459add5
Abdelrahman Diab - C43071 <abdelrahman.diab@microchip.com>
2019-05-20 10:45:30 -0700

    add beats to main page

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,6 +2,10 @@ $(document).ready(function () {
   var timeData = [],
     temperatureData = [],
     heartrateData = [];
+  var heartbeatData = [];
+  var index = [];
+  var counter = 0; 
+  
   var data = {
     labels: timeData,
     datasets: [
@@ -29,6 +33,18 @@ $(document).ready(function () {
       }
     ]
   }
+  
+    var dataset= {
+    labels: index,
+    datasets: [{ 
+        data: heartbeatData,
+        label: "heart beats",
+        borderColor: "#3e95cd",
+        fill: false
+      }
+    ]
+  }
+
 
   var basicOption = {
     title: {
@@ -67,15 +83,40 @@ $(document).ready(function () {
     }
   }
 
+  var basicOptionHeartBeats = {
+    title: {
+      display: true,
+      text: 'Heart beats Real-time Data',
+      fontSize: 36
+    },
+	scales: {
+      yAxes: [{
+        id: 'beats',
+        type: 'linear',
+		ticks: {
+                max: 500,
+                min: -500,
+                stepSize: 100
+            }
+      }]
+	}
+  }
   //Get the context of the canvas element we want to select
   var ctx = document.getElementById("myChart").getContext("2d");
+  var ctx2 = document.getElementById("myChart2").getContext("2d");
+  
   var optionsNoAnimation = { animation: false }
   var myLineChart = new Chart(ctx, {
     type: 'line',
     data: data,
     options: basicOption
   });
-
+  var myLineChart2 = new Chart(ctx2, {
+    type: 'line',
+    data: dataset,
+    options: basicOptionHeartBeats
+  });
+  
   var ws = new WebSocket('wss://' + location.host);
   ws.onopen = function () {
     console.log('Successfully connect WebSocket');
@@ -105,6 +146,22 @@ $(document).ready(function () {
         heartrateData.shift();
       }
 
+	// only keep no more than 50 points in the line chart
+      const beatmaxLen = 1000;
+
+      if (obj.beats) {
+		for (property in obj.beats) {
+			index.push(counter);
+			heartbeatData.push(obj.beats[property]);
+			counter = counter + 1;
+		}
+      }
+      if (heartbeatData.length > beatmaxLen) {
+        heartbeatData.shift();
+      }
+
+      myLineChart.update();
+	  
       myLineChart.update();
     } catch (err) {
       console.error(err);
